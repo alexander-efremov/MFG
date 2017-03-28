87ccc157aad4a107b01e7866641c04a4f40ec2f1
Alex Efremov <aedampir@gmail.com>
2017-03-28 23:46:32 +0700

    -

--- malgo/malgo.h
+++ malgo/malgo.h
@@ -81,9 +81,9 @@ inline void thomas_algo(int n, double *matrix, double *f, double *res) {
 
 /**
 	 * n - число уравнений (строк матрицы)
-	 * b - диагональ, лежащая над главной (нумеруется: [0;n-2])
-	 * c - главная диагональ матрицы A (нумеруется: [0;n-1])
 	 * a - диагональ, лежащая под главной (нумеруется: [1;n-1])
+	 * c - главная диагональ матрицы A (нумеруется: [0;n-1])
+	 * b - диагональ, лежащая над главной (нумеруется: [0;n-2])
 	 * f - правая часть (столбец)
 	 * x - решение, массив x будет содержать ответ
 	 */
@@ -100,6 +100,34 @@ inline void thomas_algo(int n, double *a, double *c, double *b, double *f, doubl
     for (int i = n - 2; i >= 0; i--)
         x[i] = (f[i] - b[i] * x[i + 1]) / c[i];
 
+}
+
+inline void thomas_algo(double *x, int X, double *a, double *b, double *c) {
+    /*
+     solves Ax = v where A is a tridiagonal matrix consisting of vectors a, b, c
+     x - initially contains the input vector v, and returns the solution x. indexed from 0 to X - 1 inclusive
+     X - number of equations (length of vector x)
+     a - subdiagonal (means it is the diagonal below the main diagonal), indexed from 1 to X - 1 inclusive
+     b - the main diagonal, indexed from 0 to X - 1 inclusive
+     c - superdiagonal (means it is the diagonal above the main diagonal), indexed from 0 to X - 2 inclusive
+
+     Note: contents of input vector c will be modified, making this a one-time-use function (scratch space can be allocated instead for this purpose to make it reusable)
+     Note 2: We don't check for diagonal dominance, etc.; this is not guaranteed stable
+     */
+
+    c[0] = c[0] / b[0];
+    x[0] = x[0] / b[0];
+
+    /* loop from 1 to X - 1 inclusive, performing the forward sweep */
+    for (size_t ix = 1; ix < X; ix++) {
+        const double m = 1.0f / (b[ix] - a[ix] * c[ix - 1]);
+        c[ix] = c[ix] * m;
+        x[ix] = (x[ix] - a[ix] * x[ix - 1]) * m;
+    }
+
+    /* loop from X - 2 to 0 inclusive (safely testing loop condition for an unsigned integer), to perform the back substitution */
+    for (int ix = X - 2; ix >= 0; ix--)
+        x[ix] -= c[ix] * x[ix + 1];
 }
 
 #endif //MFG_MALGO_H
--- tests/tests/malgo_tests.cpp
+++ tests/tests/malgo_tests.cpp
@@ -122,11 +122,7 @@ TEST_F(MalgoFixture, thomas_algo_test2) {
     ASSERT_NEAR(sol[1], 1., 1e-2);
     ASSERT_NEAR(sol[2], 1., 1e-2);
 
-    printf("\n");
-    for (int i = 0; i < n; ++i) {
-        printf("%f ", sol[i]);
-    }
-    printf("\n");
+    print_matrix(sol, 1, n);
 
     free(f);
     free(arr);
@@ -140,45 +136,39 @@ TEST_F(MalgoFixture, thomas_algo_test3) {
     double *c = (double *) malloc(n * sizeof(double));
     double *rp = (double *) malloc(n * sizeof(double));
     double *m = (double *) malloc(n * sizeof(double));
-
-//    arr[0] = 1;
-//    arr[1] = 1;
-//    arr[2] = 0;
-//    arr[3] = 1;
-//    arr[4] = 2;
-//    arr[5] = 1;
-//    arr[6] = 0;
-//    arr[7] = 1;
-//    arr[8] = 3;
-
-    a[0] = 1;
-    a[1] = 1;
-    a[2] = 0;
-    b[0] = 1;
-    b[1] = 0;
-    b[2] = 0;
-    c[0] = 1;
-    c[1] = 2;
-    c[2] = 3;
-
-    rp[0] = 2;
-    rp[1] = 4;
-    rp[2] = 4;
-
-    printf("\n");
-    print_vector(rp, n);
-
-    thomas_algo(n, a, c, b, rp, m);
-
-    ASSERT_NEAR(m[0], 1., 1e-2);
-    ASSERT_NEAR(m[1], 1., 1e-2);
-    ASSERT_NEAR(m[2], 1., 1e-2);
-
-    printf("\n");
     for (int i = 0; i < n; ++i) {
-        printf("%rp ", m[i]);
+        a[i] = b[i] = c[i] = rp[i] = m[i] = 0.;
     }
+
+    //под
+    a[0] = 0;
+    a[1] = 5;
+    a[2] = 1;
+
+    //главная
+    b[0] = 2;
+    b[1] = 4;
+    b[2] = -3;
+
+    //над
+    c[0] = -1;
+    c[1] = 2;
+    c[2] = 0;
+
+    rp[0] = 3;
+    rp[1] = 6;
+    rp[2] = 2;
+
     printf("\n");
+    print_matrix(rp, 1, n);
+
+    thomas_algo(rp, n, a, b, c);
+
+    print_matrix(rp, 1, n);
+
+    ASSERT_NEAR(rp[0], 1.49, 1e-2);
+    ASSERT_NEAR(rp[1], -0.02, 1e-2);
+    ASSERT_NEAR(rp[2], -0.68, 1e-2);
 
     free(rp);
     free(a);
@@ -186,3 +176,153 @@ TEST_F(MalgoFixture, thomas_algo_test3) {
     free(c);
 }
 
+TEST_F(MalgoFixture, thomas_algo_test4) {
+    const int n = 3;
+
+    double *a = (double *) malloc(n * sizeof(double));
+    double *b = (double *) malloc(n * sizeof(double));
+    double *c = (double *) malloc(n * sizeof(double));
+    double *rp = (double *) malloc(n * sizeof(double));
+    double *m = (double *) malloc(n * sizeof(double));
+    for (int i = 0; i < n; ++i) {
+        a[i] = b[i] = c[i] = rp[i] = m[i] = 0.;
+    }
+
+    //под
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 1;
+
+    //главная
+    b[0] = 1;
+    b[1] = 2;
+    b[2] = 3;
+
+    //над
+    c[0] = 1;
+    c[1] = 1;
+    c[2] = 0;
+
+    rp[0] = 2;
+    rp[1] = 4;
+    rp[2] = 4;
+
+    printf("\n");
+    print_matrix(rp, 1, n);
+
+    thomas_algo(rp, n, a, b, c);
+
+    print_matrix(rp, 1, n);
+
+    ASSERT_NEAR(rp[0], 1., 1e-2);
+    ASSERT_NEAR(rp[1], 1., 1e-2);
+    ASSERT_NEAR(rp[2], 1., 1e-2);
+
+    free(rp);
+    free(a);
+    free(b);
+    free(c);
+}
+
+TEST_F(MalgoFixture, thomas_algo_test5) {
+    const int n = 3;
+
+    double *a = (double *) malloc(n * sizeof(double));
+    double *b = (double *) malloc(n * sizeof(double));
+    double *c = (double *) malloc(n * sizeof(double));
+    double *rp = (double *) malloc(n * sizeof(double));
+    double *m = (double *) malloc(n * sizeof(double));
+    for (int i = 0; i < n; ++i) {
+        a[i] = b[i] = c[i] = rp[i] = m[i] = 0.;
+    }
+
+    //под
+    a[0] = 0;
+    a[1] = 5;
+    a[2] = 1;
+
+    //главная
+    c[0] = 2;
+    c[1] = 4;
+    c[2] = -3;
+
+    //над
+    b[0] = -1;
+    b[1] = 2;
+    b[2] = 0;
+
+    rp[0] = 3;
+    rp[1] = 6;
+    rp[2] = 2;
+
+    printf("\n");
+    print_matrix(rp, 1, n);
+/**
+	 * n - число уравнений (строк матрицы)
+	 * a - диагональ, лежащая под главной (нумеруется: [1;n-1])
+	 * c - главная диагональ матрицы A (нумеруется: [0;n-1])
+	 * b - диагональ, лежащая над главной (нумеруется: [0;n-2])
+	 * f - правая часть (столбец)
+	 * x - решение, массив x будет содержать ответ
+	 */
+    thomas_algo(n, a, c, b, rp, m);
+
+    print_matrix(m, 1, n);
+
+    ASSERT_NEAR(m[0], 1.49, 1e-2);
+    ASSERT_NEAR(m[1], -0.02, 1e-2);
+    ASSERT_NEAR(m[2], -0.68, 1e-2);
+
+    free(rp);
+    free(a);
+    free(b);
+    free(c);
+}
+
+TEST_F(MalgoFixture, thomas_algo_test6) {
+    const int n = 3;
+
+    double *a = (double *) malloc(n * sizeof(double));
+    double *b = (double *) malloc(n * sizeof(double));
+    double *c = (double *) malloc(n * sizeof(double));
+    double *rp = (double *) malloc(n * sizeof(double));
+    double *m = (double *) malloc(n * sizeof(double));
+    for (int i = 0; i < n; ++i) {
+        a[i] = b[i] = c[i] = rp[i] = m[i] = 0.;
+    }
+
+    //под
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 1;
+
+    //главная
+    c[0] = 1;
+    c[1] = 2;
+    c[2] = 3;
+
+    //над
+    b[0] = 1;
+    b[1] = 1;
+    b[2] = 0;
+
+    rp[0] = 2;
+    rp[1] = 4;
+    rp[2] = 4;
+
+    printf("\n");
+    print_matrix(rp, 1, n);
+
+    thomas_algo(n, a, c, b, rp, m);
+
+    print_matrix(m, 1, n);
+
+    ASSERT_NEAR(m[0], 1., 1e-2);
+    ASSERT_NEAR(m[1], 1., 1e-2);
+    ASSERT_NEAR(m[2], 1., 1e-2);
+
+    free(rp);
+    free(a);
+    free(b);
+    free(c);
+}
